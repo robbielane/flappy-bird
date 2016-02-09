5b731254ec8bfc941d8a21d87c6ff29de8088085
Robbie Lane <lane.robbie@gmail.com>
2016-02-08 17:33:05 -0700

    jorge object moveUp and moveDown

--- test/jorge-test.js
+++ test/jorge-test.js
@@ -7,7 +7,15 @@ describe('Jorge', function() {
   it('should increment one', function () {
     var jorge = new Jorge(50,50);
     jorge.moveUp;
+
     assert.equal(51, jorge.y);
+  });
+
+  it('should decrement one', function () {
+    var jorge = new Jorge(50,50);
+    jorge.moveDown;
+
+    assert.equal(49, jorge.y);
   });
 
 });
