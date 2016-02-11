7505ae136442cb922de04156a190619e010de38f
rossedfort <rossedfort@gmail.com>
2016-02-10 18:28:10 -0700

    stuck on testing event listeners - moving to lose sequence

--- test/collision-test.js
+++ test/collision-test.js
@@ -4,55 +4,28 @@ var Collision = require('../lib/collision');
 var Bird = require('../lib/bird');
 var Pipe = require('../lib/pipe');
 var Ground = require('../lib/ground');
-var EventEmitter = require('events');
-var e = new EventEmitter();
+const sinon = require('sinon/pkg/sinon');
 
 describe('Collision', function(){
   context('detection', function(){
-    var bird = new Bird(50, 50, 50, 57);
-    var pipe = new Pipe(context, 101);
-    var ground = new Ground(context, 0)
-    var collision = new Collision(bird, pipe, ground);
+    xit('when bird hits top pipe', function(){
+      var bird = new Bird(50, 50, 50, 57, context);
+      var pipes = [new Pipe(context, 101)];
+      var grounds = [new Ground(context, 0)];
+      var collision = new Collision(bird, pipes, grounds);
 
-    it('when bird hits top pipe', function(){
-      var eventEmitted = false
-      e.on('collisionEvent', function() {
-        eventEmitted = true;
+      var birdPipeCollision = { collisionEvent: function detect(){} };
+      var spy = sinon.spy(birdPipeCollision, 'collisionEvent');
+
+      collision.detect;
+      pipes.forEach( (pipe) => {
+        pipe.move;
+        pipe.updateBounds();
       });
-      pipe.move;
-      pipe.updateBounds();
-      pipe.move;
-      pipe.updateBounds();
-      pipe.move;
-      pipe.updateBounds();
-      pipe.move;
-      pipe.updateBounds();
-      pipe.move;
-      pipe.updateBounds();
-      pipe.move;
-      pipe.updateBounds();
-      pipe.move;
-      pipe.updateBounds();
-      pipe.move;
-      pipe.updateBounds();
-      pipe.move;
-      pipe.updateBounds();
-      pipe.move;
-      pipe.updateBounds();
-      pipe.move;
-      pipe.updateBounds();
-      pipe.move;
-      pipe.updateBounds();
-      pipe.move;
-      pipe.updateBounds();
-      pipe.move;
-      pipe.updateBounds();
-
-      assert.equal(true, eventEmitted);
+      assert(spy.calledOnce, 'event was not fired');
     });
 
-    it('when bird hits bottom pipe', function(){
-
+    xit('when bird hits bottom pipe', function(){
     });
   });
 });
